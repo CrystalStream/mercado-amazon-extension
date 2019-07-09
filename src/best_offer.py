2163863b6d3b3a68ca9a1541614637abc152d432
Christofer Flores <cristofer.flort@gmail.com>
2019-07-09 18:50:03 -0500

    Parse the ml product, still issues on how to find the image

--- src/best_offer.py
+++ src/best_offer.py
@@ -1,3 +1,5 @@
+import re
+
 def get_ml_products(html):
     items = []
     if html is None:
@@ -6,7 +8,7 @@ def get_ml_products(html):
     product_list = html.find('ol', id="searchResults")
     if product_list:
         for item in product_list.find_all('li', class_="results-item"):
-                items.append(str(item))
+                items.append(parse_ml_item(item))
     return items
 
 def get_amzn_products(html):
@@ -23,3 +25,28 @@ def get_amzn_products(html):
 
 def gather_results(ml, amzn=None):
     return [get_ml_products(ml), get_amzn_products(amzn)]
+
+def search(container, rules):
+    rules = rules.split('.')
+    while len(rules) > 1:
+        rule = rules[0]
+        attrs = re.findall('\[(.*?)\]', rule)
+        if len(attrs):
+            attr, value = attrs[0].split('=')
+            rule = rules[0].replace('[{}]'.format(attrs[0]), '')
+            parent = container.find(rule, **{attr: value})
+        else:
+            parent = container.find(rules[0])
+
+        return search(parent, '.'.join(rules[1:]))
+
+    return container.find(rules[0])
+
+
+def parse_ml_item(container):
+    img = search(container, 'ul.li.img')
+    return {
+        'name': img.get('title', img.get('alt', '')),
+        'img': img.get('src', '')
+    }
+
